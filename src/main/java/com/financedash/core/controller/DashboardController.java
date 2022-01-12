a0e10ece8aeceeea2dd39be1524bdd3a19817a43
Angel Stoyanov <aststoyanov@gmail.com>
2022-01-12 17:16:46 +0200

    [Update] Add Auth principle to every template, add dynamic fields from the Model

--- src/main/java/com/financedash/core/controller/DashboardController.java
+++ src/main/java/com/financedash/core/controller/DashboardController.java
@@ -9,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Controller
 public class DashboardController {
@@ -31,14 +33,20 @@ public class DashboardController {
     }
 
     @GetMapping("/transactions")
-    public String table(Model model) {
+    public String table(Model model, OAuth2AuthenticationToken authentication) {
         model.addAttribute("earnings", 0);
+        model.addAttribute("given_name", authentication.getPrincipal().getAttributes().get("given_name"));
+        model.addAttribute("family_name", authentication.getPrincipal().getAttributes().get("family_name"));
         return "table";
     }
 
     @GetMapping("/profile")
-    public String profile(Model model) {
-        model.addAttribute("earnings", 0);
+    public String profile(Model model, OAuth2AuthenticationToken authentication) {
+        model.addAttribute("given_name", authentication.getPrincipal().getAttributes().get("given_name"));
+        model.addAttribute("family_name", authentication.getPrincipal().getAttributes().get("family_name"));
+        //System.out.println(((com.nimbusds.jose.shaded.json.JSONArray) authentication.getPrincipal().getAttributes().get("emails")).toJSONString());
+        //Matcher matcher = Pattern.compile("\\[\"(.*)\"\\]").matcher(((com.nimbusds.jose.shaded.json.JSONArray) authentication.getPrincipal().getAttributes().get("emails")).toJSONString());
+       model.addAttribute("email", (((com.nimbusds.jose.shaded.json.JSONArray) authentication.getPrincipal().getAttributes().get("emails")).toJSONString()));
         return "profile";
     }
 }
