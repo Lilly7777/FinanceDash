f92fa132df189d264d202f27079ab76d17c8307c
Angel Stoyanov <aststoyanov@gmail.com>
2022-01-17 22:33:37 +0200

    [Update] Fix Resource server call from Dashboard Controller and add call from templates

--- src/main/java/com/financedash/core/controller/DashboardController.java
+++ src/main/java/com/financedash/core/controller/DashboardController.java
@@ -1,28 +1,25 @@
 package com.financedash.core.controller;
 
 import com.financedash.core.model.Transaction;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
+@CrossOrigin(origins = "*")
 @Controller
 public class DashboardController {
 
@@ -46,6 +43,9 @@ public class DashboardController {
         model.addAttribute("tasks_count", 0);
         model.addAttribute("given_name", authentication.getPrincipal().getAttributes().get("given_name"));
         model.addAttribute("family_name", authentication.getPrincipal().getAttributes().get("family_name"));
+        String email = StringUtils.replace((((com.nimbusds.jose.shaded.json.JSONArray) authentication.getPrincipal().getAttributes().get("emails")).toJSONString()), "[\"", "");
+        email = StringUtils.replace(email, "\"]", "");
+        model.addAttribute("email", email);
 
 
         return "index";
@@ -57,6 +57,10 @@ public class DashboardController {
         OAuth2AuthorizedClient client = oAuth2AuthorizedClientService
                 .loadAuthorizedClient(authentication.getAuthorizedClientRegistrationId(), authentication.getName());
 
+        String email = StringUtils.replace((((com.nimbusds.jose.shaded.json.JSONArray) authentication.getPrincipal().getAttributes().get("emails")).toJSONString()), "[\"", "");
+        email = StringUtils.replace(email, "\"]", "");
+        model.addAttribute("email", email);
+
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(client.getAccessToken().getTokenValue());
@@ -64,14 +68,16 @@ public class DashboardController {
 
         HttpEntity<List<Transaction>> httpEntity = new HttpEntity<>(null, headers);
 
-        ResponseEntity<List> response = restTemplate.exchange(resourceServerUri + "/api/v1/transaction", HttpMethod.GET, httpEntity, List.class, Map.of("userId", "test"));
+        ResponseEntity<List> response = restTemplate.exchange(resourceServerUri + "/api/v1/transaction?userId={name}", HttpMethod.GET, httpEntity, List.class, email);
         List<Transaction> transactions = response.getBody();
+
         //List<Transaction> transactions = (List<Transaction>) restTemplate.getForObject(resourceServerUri + "", List.class);
 
 
         model.addAttribute("earnings", 0);
         model.addAttribute("given_name", authentication.getPrincipal().getAttributes().get("given_name"));
         model.addAttribute("family_name", authentication.getPrincipal().getAttributes().get("family_name"));
+        model.addAttribute("transactions", transactions);
         return "table";
     }
 
@@ -81,7 +87,9 @@ public class DashboardController {
         model.addAttribute("family_name", authentication.getPrincipal().getAttributes().get("family_name"));
         //System.out.println(((com.nimbusds.jose.shaded.json.JSONArray) authentication.getPrincipal().getAttributes().get("emails")).toJSONString());
         //Matcher matcher = Pattern.compile("\\[\"(.*)\"\\]").matcher(((com.nimbusds.jose.shaded.json.JSONArray) authentication.getPrincipal().getAttributes().get("emails")).toJSONString());
-       model.addAttribute("email", (((com.nimbusds.jose.shaded.json.JSONArray) authentication.getPrincipal().getAttributes().get("emails")).toJSONString()));
+        String email = StringUtils.replace((((com.nimbusds.jose.shaded.json.JSONArray) authentication.getPrincipal().getAttributes().get("emails")).toJSONString()), "[\"", "");
+        email = StringUtils.replace(email, "\"]", "");
+        model.addAttribute("email", email);
         return "profile";
     }
 }
