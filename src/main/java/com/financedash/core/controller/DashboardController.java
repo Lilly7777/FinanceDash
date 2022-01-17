ac8f2eb61932be8c31e7859d2b18dbcfb740b2f5
Liliya Lyubenova <lilly.lubenova@gmail.com>
2022-01-17 20:44:41 +0200

    [Update] Receive Access token from the current authorized client and save it as a cookie

--- src/main/java/com/financedash/core/controller/DashboardController.java
+++ src/main/java/com/financedash/core/controller/DashboardController.java
@@ -1,13 +1,19 @@
 package com.financedash.core.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,11 +21,16 @@ import java.util.regex.Pattern;
 @Controller
 public class DashboardController {
 
+    @Autowired
+    OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
+
     @GetMapping("/")
-    public String index(Model model, OAuth2AuthenticationToken authentication) {
+    public String index(Model model, OAuth2AuthenticationToken authentication, HttpServletResponse response) {
 
+        OAuth2AuthorizedClient client = oAuth2AuthorizedClientService
+                .loadAuthorizedClient(authentication.getAuthorizedClientRegistrationId(), authentication.getName());
 
-        System.out.println(authentication.getPrincipal().getAttributes());
+        response.addCookie(new Cookie("ACCESS_TOKEN", client.getAccessToken().getTokenValue()));
 
         model.addAttribute("earnings", 0);
         model.addAttribute("expenses", 0);
